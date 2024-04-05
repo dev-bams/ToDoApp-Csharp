3629bfb6c0b2a034e7bbf44494aa5c2f1e7ba428
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-04-04 20:34:02 -0400

    progress

--- Program.cs
+++ Program.cs
@@ -10,7 +10,65 @@ namespace ToDoApp
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hi there");
+      while (true)
+      {
+        DisplayMenu();
+        string userInput = GetUserMenuInput();
+        RenderMenuBasedOnUserInput(userInput);
+      }
+    }
+
+    static void DisplayMenu()
+    {
+      Console.Clear();
+      Console.WriteLine("Welcome to Taskpulse\n");
+      Console.WriteLine("This simple application allows you to manage your tasks right from the command line\n");
+      Console.WriteLine("Instructions:\n");
+      Console.WriteLine("- To add a new task, enter '1'.\n");
+      Console.WriteLine("- To view your tasks, enter '2'.\n");
+      Console.WriteLine("- To mark a task as completed, enter '3'.\n");
+      Console.WriteLine("- To exit the application, enter '4'.\n\n");
+      Console.WriteLine("Let's get started!\n");
+    }
+    static string GetUserMenuInput()
+    {
+      Console.Write("Choice: ");
+      string userChoice = Console.ReadLine();
+      return userChoice;
+    }
+    static void RenderMenuBasedOnUserInput(string userChoice)
+    {
+      Console.Clear();
+      switch (userChoice)
+      {
+        case "1":
+          CreateNewTask();
+          break;
+      }
+    }
+    static void CreateNewTask()
+    {
+      Console.Write("Enter number of tasks: ");
+      int numberOfTasks = int.Parse(Console.ReadLine());
+      Console.WriteLine("\n");
+      TodoItem[] todoItems = new TodoItem[numberOfTasks];
+      for (int i = 0; i < numberOfTasks; i++)
+      {
+        Console.WriteLine($"Task{i + 1}");
+        Console.Write("Task title: ");
+        string title = Console.ReadLine();
+        Console.Write("Task description: ");
+        string description = Console.ReadLine();
+        Console.Write("Task due date: ");
+        string dueDate = Console.ReadLine();
+        Console.Write("Task priority: ");
+        string priority = Console.ReadLine();
+        Console.Write("Task status: ");
+        string status = Console.ReadLine();
+        Console.WriteLine("\n");
+        TodoItem todoItem = new TodoItem(title, description, dueDate, priority, status);
+        todoItems[i] = todoItem;
+      }
     }
   }
 }
---
+++ TodoItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp
+{
+  internal class TodoItem
+  {
+    public static int id;
+    public string title;
+    public string description;
+    public string dueDate;
+    public string priority;
+    public string status;
+
+    public TodoItem(string title, string description, string dueDate, string priority, string status)
+    {
+      this.title = title;
+      this.description = description;
+      this.dueDate = dueDate;
+      this.priority = priority;
+      this.status = status;
+      id += 1;
+    }
+  }
+}
